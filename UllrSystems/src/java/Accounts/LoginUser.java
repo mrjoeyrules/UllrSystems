9784403aa0aff63f1a85a5c60d0bab418556d1c9
mrjoeyrules <mrjoeyrules@gmail.com>
2025-01-20 22:19:08 +0000

    Added Login logs and logging tool in sqlinterfacing

--- UllrSystems/src/java/Accounts/LoginUser.java
+++ UllrSystems/src/java/Accounts/LoginUser.java
@@ -12,7 +12,6 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 /**
  *
  * @author mrjoeyrules
@@ -21,10 +20,9 @@ import java.util.logging.Logger;
 public class LoginUser extends HttpServlet {
 
 // Map the servlet to "processData" (matches the form action)
-
     private static final long serialVersionUID = 1L;
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
         SQLInterfacing sql = new SQLInterfacing();
@@ -32,18 +30,22 @@ public class LoginUser extends HttpServlet {
         String rawPassword = request.getParameter("password"); // gets password from form
         try {
             boolean isLoggedIn = sql.AuthenticateUserFromDB(username, rawPassword); // runs password check from sql
-            if(isLoggedIn){
+            if (isLoggedIn) {
                 session.setAttribute("username", User.currentUser.getUsername()); // sets session attribute username to username to allow for unfiltered access to site
                 response.sendRedirect("MainMenu.html"); // if logged in correct send to next page
-            }
-            else{
+            } else {
+                try {
+                    sql.WriteLog(("User " + username + " attempted to login but password was incorrect"), 1);
+                } catch (SQLException ex) {
+                    Logger.getLogger(LoginUser.class.getName()).log(Level.SEVERE, null, ex);
+                }
                 response.sendRedirect("index.html?error=Username+or+Password+is+incorrect."); // if incorrect send error to page
             }
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(LoginUser.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
+
 }
