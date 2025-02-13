3ca933ff1d6e6ae8fc8ca824ff5218835fd3abcb
Amromer23 <amrukps@gmail.com>
2025-02-13 03:38:24 +0000

    Alerts Methods

---
+++ UllrSystems/src/java/Databasing/Alerts.java
@@ -0,0 +1,51 @@
+package Databasing;
+
+import java.time.LocalDateTime;
+
+public class Alerts {
+    private int alertId;
+    private String alertMsg;
+    private LocalDateTime timestamp;
+    private boolean markedAsRead;
+    private int alertType;
+
+    public int getAlertId() {
+        return alertId;
+    }
+
+    public void setAlertId(int alertId) {
+        this.alertId = alertId;
+    }
+
+    public String getAlertMsg() {
+        return alertMsg;
+    }
+
+    public void setAlertMsg(String alertMsg) {
+        this.alertMsg = alertMsg;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isMarkedAsRead() {
+        return markedAsRead;
+    }
+
+    public void setMarkedAsRead(boolean markedAsRead) {
+        this.markedAsRead = markedAsRead;
+    }
+
+    public int getAlertType() {
+        return alertType;
+    }
+
+    public void setAlertType(int alertType) {
+        this.alertType = alertType;
+    }
+}
