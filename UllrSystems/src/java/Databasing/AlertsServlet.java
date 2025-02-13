f907e40ca8b9d2e2f162f26fbff51873cae79065
Amromer23 <amrukps@gmail.com>
2025-02-13 03:38:17 +0000

    Alerts Methods

---
+++ UllrSystems/src/java/Databasing/AlertsServlet.java
@@ -0,0 +1,55 @@
+package Databasing;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/api/alerts")
+public class AlertsServlet extends HttpServlet {
+
+    private final SQLInterfacing dbInterface = new SQLInterfacing();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            dbInterface.checkExpiringFood();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<Alerts> allAlerts = new ArrayList<>();
+        try {
+            allAlerts = dbInterface.GetAllAlerts();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        out.print("[");
+        for (int i = 0; i < allAlerts.size(); i++) {
+            Alerts alert = allAlerts.get(i);
+
+            out.print("{");
+            out.print("\"alertId\":" + alert.getAlertId() + ",");
+            out.print("\"alertMsg\":\"" + alert.getAlertMsg().replace("\"","\\\"") + "\",");
+            out.print("\"timestamp\":\"" + alert.getTimestamp() + "\",");
+            out.print("\"markedAsRead\":" + alert.isMarkedAsRead() + ",");
+            out.print("\"alertType\":" + alert.getAlertType());
+            out.print("}");
+
+            if (i < allAlerts.size() - 1) {
+                out.print(",");
+            }
+        }
+        out.print("]");
+        out.flush();
+    }
+}
